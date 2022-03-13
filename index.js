8096dd14d260b5d8671759e88a61c1056f1885b4
Keven Banuelos <kevenbanuelos@gmail.com>
2022-03-13 14:58:46 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction ()
+function introduction(Bob){
+    return("Hi, my name is ${name}")
+}
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+  I am so lost
